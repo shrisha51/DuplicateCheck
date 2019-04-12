aff27591242962b34f8d6d6e045ee196b90149f9
i339568 <shri.raksha.nagaraj@sap.com>
2019-04-13 00:28:12 +0530

    included check on partner repo

--- src/main/java/com/example/duplicateCheck/DuplicateCheckService.java
+++ src/main/java/com/example/duplicateCheck/DuplicateCheckService.java
@@ -17,46 +17,16 @@ public class DuplicateCheckService {
 	private final AtomicLong counter = new AtomicLong();
     
 
-	public Iterable<ResponseMatrix>  checkDuplicate(String name, String email, String mobile) {
+	public Iterable<ResponseMatrix> checkDuplicate(String name, String email, String mobile) {
 		
        
         StringBuilder sbi = new StringBuilder();
-        duplicateCheckonList(false,name,email,mobile);
-        
-//        iall.forEach(p ->
-//        {
-//        	boolean matched=false;
-//        	int Strength=0;
-//        	if(p.getName().equalsIgnoreCase(name)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Name");
-//        		Strength+=1;
-//        	}
-//        	if(p.getMobile().equalsIgnoreCase(mobile)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Mobile");
-//        		Strength+=3;
-//        	}
-//        	if(p.getEmail().equalsIgnoreCase(email)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Email");
-//        		Strength+=5;
-//        	}
-//        	if(matched) {
-//        	ResponseMatrix p5=new ResponseMatrix(counter.incrementAndGet(),p.getName(),p.getEmail(),p.getMobile(),"Prior Invitation","",MatchedOn.toString(),Strength);
-//        	//sbi.append(p.getName() + "<br>");
-//        	responseMatrixDAO.save(p5);
-//        	matched=false;
-//        	Strength=0;
-//        	MatchedOn.setLength(0);
-//        	}
-//        });
-        
+        duplicateCheckonList(name,email,mobile);
         Iterable<ResponseMatrix> response= responseMatrixDAO.findAll();
         return response;
 	}
 	
-	private void duplicateCheckonList(boolean fromRepo,String name, String email, String mobile) {
+	private void duplicateCheckonList(String name, String email, String mobile) {
 		
 		Iterable<InputMatrix> iall = inputMatrixDAO.findAll(); 
 		
@@ -90,5 +60,36 @@ public class DuplicateCheckService {
 	        	MatchedOn.setLength(0);
 	        	}
 	        });
+		 Iterable<PartnerRepo> pall = partnerRepoDAO.findAll(); 
+			
+			StringBuilder MatchedOn1 = new StringBuilder();
+			 
+			 pall.forEach(p ->
+		        {
+		        	boolean matched=false;
+		        	int Strength=0;
+		        	if(p.getName().equalsIgnoreCase(name)) {
+		        		matched=true;	
+		        		MatchedOn1.append("-Name");
+		        		Strength+=1;
+		        	}
+		        	if(p.getMobile().equalsIgnoreCase(mobile)) {
+		        		matched=true;	
+		        		MatchedOn1.append("-Mobile");
+		        		Strength+=3;
+		        	}
+		        	if(p.getEmail().equalsIgnoreCase(email)) {
+		        		matched=true;	
+		        		MatchedOn1.append("-Email");
+		        		Strength+=5;
+		        	}
+		        	if(matched) {
+		        	ResponseMatrix p5=new ResponseMatrix(counter.incrementAndGet(),p.getName(),p.getEmail(),p.getMobile(),"Partner Repository",p.getNetworkId(),MatchedOn1.toString(),Strength);
+		        	responseMatrixDAO.save(p5);
+		        	matched=false;
+		        	Strength=0;
+		        	MatchedOn1.setLength(0);
+		        	}
+		        });
 	}
 }
--- src/main/java/com/example/duplicateCheck/InvitationController.java
+++ src/main/java/com/example/duplicateCheck/InvitationController.java
@@ -11,66 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InvitationController {
-	  
-	@Autowired
-    private InputMatrixDAO inputMatrixDAO;
-	@Autowired
-	private ResponseMatrixDAO responseMatrixDAO;
+
 	@Autowired
 	private DuplicateCheckService service;
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-    
-
+ 
     @RequestMapping("/sendInvite")
     public Iterable<ResponseMatrix> greeting(@RequestParam(value="name", defaultValue="World") String name,@RequestParam(value="email", defaultValue="World") String email,@RequestParam(value="mobile", defaultValue="World") String mobile) {
-//        return new InputMatrix(
-//                            String.format(template, name),String.format(template, email),String.format(template, mobile));
-
-//    	Iterable<InputMatrix> iall = inputMatrixDAO.findAll();
-//        StringBuilder MatchedOn = new StringBuilder();
-//        StringBuilder sbi = new StringBuilder();
-//        
-//        iall.forEach(p ->
-//        {
-//        	boolean matched=false;
-//        	int Strength=0;
-//        	if(p.getName().equalsIgnoreCase(name)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Name");
-//        		Strength+=1;
-//        	}
-//        	if(p.getMobile().equalsIgnoreCase(mobile)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Mobile");
-//        		Strength+=3;
-//        	}
-//        	if(p.getEmail().equalsIgnoreCase(email)) {
-//        		matched=true;	
-//        		MatchedOn.append("-Email");
-//        		Strength+=5;
-//        	}
-//        	if(matched) {
-//        	ResponseMatrix p5=new ResponseMatrix(counter.incrementAndGet(),p.getName(),p.getEmail(),p.getMobile(),"Prior Invitation","",MatchedOn.toString(),Strength);
-//        	//sbi.append(p.getName() + "<br>");
-//        	responseMatrixDAO.save(p5);
-//        	matched=false;
-//        	Strength=0;
-//        	MatchedOn.setLength(0);
-//        	}
-//        });
+       
 
         return service.checkDuplicate(name,email,mobile);
         
-       // InputMatrix inp=new InputMatrix(counter.incrementAndGet(),name,email,mobile);
         //ResponseMatrix p5=new ResponseMatrix(counter.incrementAndGet(),"ASDASD",name,email,mobile,"SADASD","Something",1);
-        //inputMatrixDAO.save(inp);
        // responseMatrixDAO.save(p5);
         	
-        
-        //sb.append(sbi.toString());
-    	//return new ResponseMatrix(counter.incrementAndGet(),"ASDASD",name,email,mobile,"SADASD",MatchedOn.toString(),1);
-//        Iterable<ResponseMatrix> response= responseMatrixDAO.findAll();
-//        return response;
+      
     }
 }
